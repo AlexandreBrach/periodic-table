341a5960d48c02d29b9b35db913d99ceb3b18709
Alexandre BRACH <alexandre.brach@gmail.fr>
2026-08-28 19:16:33 +0200

    chore: rename app-logo to Atom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="Atom" alt="logo" />
     </div>
   );
 }
